33601b24174bad1601e271bde93139f0e8c797dc
la-gg <128537018+la-gg@users.noreply.github.com>
2023-03-30 11:04:51 -0500

    Added live lab lecture 4

---
+++ lab - lecture 4.py
@@ -0,0 +1,14 @@
+#Convert these objects!
+
+#1
+start_list = [[2, 3, 4], [6, 8, 9]]
+#turn it into [1,    2,   3, 4   ]
+
+#2
+import datetime
+start_dict = {'noah': '2/23/1999',
+              'sarah':'9/1/2001',
+              'zach': '8/8/2005'}
+#turn it into {'Noah': datetime(1999, 2, 23),
+#              'Sarah':datetime(2001, 9, 1),
+#              'Zach': datetime(2005, 8, 8)}
