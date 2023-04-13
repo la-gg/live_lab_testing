0c97cbb5f4e369bc8f56ee0db1f2f5f3acb8c1d8
la-gg <128537018+la-gg@users.noreply.github.com>
2023-04-13 11:20:18 -0500

    I think i've done this the wrong way

--- lab - lecture 8.py
+++ lab - lecture 8.py
@@ -3,6 +3,14 @@
 from numpy import random
 
 choices = ['rock', 'paper', 'scissors']
-
-p1 = random.choice(choices)
-p2 = random.choice(choices)
+#To start, need to make a function
+def rock_paper_scissors(p1, p2):
+    # Args
+    p1 = random.choice(choices)
+    p2 = random.choice(choices)
+    # If you get the same thing
+    if p1 == p2:
+        print('Go again.')
+        pass
+    # Need to say what happens for someone to win
+    elif 
