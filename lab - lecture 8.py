cf822591d00e092d96f6408b4cca4d2195a2da00
la-gg <128537018+la-gg@users.noreply.github.com>
2023-04-13 11:37:28 -0500

    Writing a Function to Decide who wins

--- lab - lecture 8.py
+++ lab - lecture 8.py
@@ -3,14 +3,37 @@
 from numpy import random
 
 choices = ['rock', 'paper', 'scissors']
-#To start, need to make a function
-def rock_paper_scissors(p1, p2):
-    # Args
-    p1 = random.choice(choices)
-    p2 = random.choice(choices)
-    # If you get the same thing
-    if p1 == p2:
-        print('Go again.')
+p1 = random.choice(choices)
+p2 = random.choice(choices)
+
+# First things first, how does someone win the game (paper beats rock, rock 
+# beats scissors, scissors beats paper)
+# ok lets start with who wins
+def wins(rock, paper, scissors):
+    # begin by saying which wins in the three straightforward scenarios
+    if rock and paper:
+        return('paper wins')
         pass
-    # Need to say what happens for someone to win
-    elif 
+    elif paper and scissors:
+        return('scissors wins')
+        pass
+    elif scissors and rock:
+        return('rock wins')
+        pass
+    # what happens if it is a draw
+    else:
+        return('Go again')
+        pass
+def rps (p1, p2):
+    print('Rock, paper, scissors, go!')
+    # so I want both p1 and p2 to state their choices
+    
+    # then I want to call wins
+    # then I want it to repeat if 'Go again'
+    # then I want it to state who won of p1 or p2
+    
+    
+    
+        
+    
+
